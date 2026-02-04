fab67fbfacd9033f477f9173f75597d88bd18122
Hadi Movaghar <hadi.movaghar@verimi.com>
2026-02-04 14:31:45 +0100

    Kitchen service original

--- src/main/java/com/verimi/kitchenservice/repository/StaffMemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.verimi.kitchenservice.repository;
-
-import com.verimi.kitchenservice.repository.entity.StaffMember;
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StaffMemberRepository extends CrudRepository<StaffMember, Long> {
-    List<StaffMember> findAll();
-}
